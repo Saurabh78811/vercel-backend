97bfbc4df40a698a9f694380a8e8dd1a71decb66
Saurabh verma <87320652+Saurabh78811@users.noreply.github.com>
2025-10-23 00:08:31 +0530

    l

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000; // ✅ Added fallback for safety
 // ✅ Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
